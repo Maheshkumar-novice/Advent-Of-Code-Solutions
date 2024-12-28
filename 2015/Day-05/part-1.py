83ddd839f677ccbe2e4e4da6bd3494d8ae95c34b
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2024-12-29 00:45:33 +0530

    Make 2015:5 even shorter lol

--- 2015/Day-05/part-1.py
+++ 2015/Day-05/part-1.py
@@ -4,4 +4,4 @@ with open("input.txt") as f:
     vowels = {"a", "e", "i", "o", "u"}
     doubles = r"(.)\1"
     unwanted = r"(ab|cd|pq|xy)"
-    print(sum(1 for string in f.read().splitlines() if not re.findall(unwanted, string) and re.findall(doubles, string) and sum(1 for char in string if char in vowels) >= 3))  # noqa: PLR2004
+    print(sum(bool(not re.findall(unwanted, string) and re.findall(doubles, string) and sum(char in vowels for char in string) >= 3) for string in f.read().splitlines()))  # noqa: PLR2004
--- 2015/Day-05/part-2.py
+++ 2015/Day-05/part-2.py
@@ -3,4 +3,4 @@ import re
 with open("input.txt") as f:
     doubles_twice = r"([a-z]{2})(.*)(\1)"
     doubles_inbetween = r"([a-z]{1})(.)(\1)"
-    print(sum(1 for string in f.read().splitlines() if re.findall(doubles_twice, string) and re.findall(doubles_inbetween, string)))
+    print(sum(bool(re.findall(doubles_twice, string) and re.findall(doubles_inbetween, string)) for string in f.read().splitlines()))
