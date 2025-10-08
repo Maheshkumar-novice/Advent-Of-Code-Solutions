7ca6e854591350cdaa987152a728cd75c50ba142
Maheshkumar <67100964+Maheshkumar-novice@users.noreply.github.com>
2025-10-08 18:37:23 +0530

    Add 2015:12

---
+++ 2015/Day-12/part-1.py
@@ -0,0 +1,18 @@
+import json
+
+
+def parse(object: str | int | list | dict) -> None:  # noqa: A002, D103
+    total = 0
+    if isinstance(object, int):
+        total += object
+    elif isinstance(object, list):
+        for obj in object:
+            total += parse(obj)
+    elif isinstance(object, dict):
+        for val in object.values():
+            total += parse(val)
+    return total
+
+
+with open("input.txt") as f:
+    print(parse(json.loads(f.read())))
